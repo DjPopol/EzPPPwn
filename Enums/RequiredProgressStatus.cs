b39068c1a7d9eab2111391f0256ac1f32f83340d
DjPopol <djpopol.dev@gmail.com>
2024-06-08 15:54:51 +0200

    Create RequiredProgressStatus.cs
    Required Progress Status

---
+++ Enums/RequiredProgressStatus.cs
@@ -0,0 +1,15 @@
+﻿namespace EzPPPwn.Enums
+{
+    public enum REQUIRED_PROGESS_STATUS
+    {
+        CANCELED,
+        COMPLETED,
+        COPYING,
+        DOWNLOADING,
+        EXTRACTING,
+        FAILED,
+        INSTALLING,
+        NONE,
+        STARTING,
+    };
+}
