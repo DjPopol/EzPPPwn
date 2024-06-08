9d2b7ee2a8fb4d8033444b4652d465f8049e04e7
DjPopol <djpopol.dev@gmail.com>
2024-06-08 16:04:32 +0200

    Create VersionInfos.cs
    helpers to retrieve version

---
+++ Models/VersionInfos.cs
@@ -0,0 +1,9 @@
+﻿namespace EzPPPwn.Models
+{
+    public class VersionInfos(int version, int major, int minor)
+    {
+        public int Version = version;
+        public int Major = major;
+        public int Minor = minor;
+    }
+}
