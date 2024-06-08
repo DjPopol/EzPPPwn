4d7ce852a0be7b5f78749300e00562bbf027cd7a
DjPopol <djpopol.dev@gmail.com>
2024-06-08 16:03:28 +0200

    Create JSONConfig.cs
    Add new v1.10 options

---
+++ Models/JSONConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EzPPPwn.Models
+{
+    public class JSONConfig
+    {
+        public required string Interface { get; set; }
+        public required string Firmware { get; set; }
+        public required bool ShowConsole { get; set; }
+        [JsonPropertyName("Stage2")]
+        public required string Stage2Path { get; set; }
+        [JsonPropertyName("PPPwn")]
+        public required JSONPPPwnOptions JSON_PPPwnConfig { get; set; }
+
+    }
+}
